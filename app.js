9f246fe706924a964c6dc32677ae8f219425dadc
Doğan Şengül <60586840+dogansengul@users.noreply.github.com>
2025-02-03 18:48:54 +0300

    Middleware practices

--- app.js
+++ app.js
@@ -5,10 +5,36 @@ const { PORT } = process.env;
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log(req.method, req.url);
-  res.status(200).send('Hey');
-  next();
+app.use(express.static('public'));
+
+const loggerWithPrefix = (prefix) => {
+    return function (req, res, next) {
+        console.log(`${prefix}: Method: ${req.method}, Url: ${req.url}`);
+        next();
+    };
+};
+
+let reqTime = null;
+let resTime = null;
+
+const requestTime = (req, res, next) => {
+    const dateNow = Date.now();
+    reqTime = dateNow;
+    req.requestTime = dateNow;
+    next();
+};
+
+app.use(requestTime);
+
+app.use('/', loggerWithPrefix('Info about request'));
+
+app.get('/', (req, res, next) => {
+    const dateNow = Date.now();
+    resTime = dateNow;
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(200).send(
+        `Request Time: ${reqTime}, Response Time: ${resTime}, Difference: ${resTime - reqTime} \nHello from the main page!`
+    );
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
